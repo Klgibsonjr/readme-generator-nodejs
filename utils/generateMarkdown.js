efdba8aadefd24e4640ed04e048c15333465c899
klgibsonjr <klgibsonjr@gmail.com>
2022-12-04 23:53:21 -0800

    completed renderLlicenseLink and renderLlicenseSection functionality

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,13 +8,16 @@ const index = require('../index');
 function renderLicenseBadge(license) {
   let badge = '';
   if (license === 'MIT') {
-    badge = '![GitHub License](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)'
+    badge =
+      '![GitHub License](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)';
   } else if (license === 'Apache 2.0') {
-    badge = '![Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+    badge =
+      '![Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
   } else if (license === 'GNU GPL v3.0') {
-    badge = '![GNU GPL v3.0 License](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+    badge =
+      '![GNU GPL v3.0 License](https://img.shields.io/badge/License-GPLv3-blue.svg)';
   } else {
-    badge = 'A license was not selected'
+    badge = 'A license was not selected';
   }
   return badge;
 }
@@ -24,16 +27,16 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   let licenseLink = '';
   if (license === 'MIT') {
-    licenseLink = 'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt'
+    licenseLink =
+      'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt';
   } else if (license === 'Apache 2.0') {
-    licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0'
+    licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0';
   } else if (license === 'GNU GPL v3.0') {
-    licenseLink = 'https://www.gnu.org/licenses'
+    licenseLink = 'https://www.gnu.org/licenses';
   } else {
-    licenseLink = 'A license was not selected'
+    licenseLink = 'A license was not selected';
   }
   return licenseLink;
-
 }
 
 // TODO: Create a function that returns the license section of README
@@ -43,8 +46,7 @@ function renderLicenseSection(license) {
   if (license === 'None') {
     licenseSection = '';
   } else {
-    licenseSection =
-    `License: ${license} `
+    licenseSection = `License: ${license} `;
   }
   return licenseSection;
 }
@@ -52,11 +54,6 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(input) {
   return `# ${input.title}`;
-  
 }
 
 module.exports = generateMarkdown;
-
-
-
-https://img.shields.io/github/license/klgibsonjr/readme-generator-nodejs
